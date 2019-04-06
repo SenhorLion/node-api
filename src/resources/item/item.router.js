b00b3ed0c32c1b959fd8cd71dd71c616ef18909d
Lionel Adams <adamslionel@hotmail.co.uk>
2019-04-06 22:19:06 +0100

    feat: Add routing

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const router = Router()
+const router = Router();
 
-export default router
+const controller = (req, res) => {
+  res.send({ message: 'Controller says Hello' });
+};
+// api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller);
+
+// api/item/id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .post(controller)
+  .delete(controller);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,50 +1,33 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 
-export const app = express()
+import itemRouter from './resources/item/item.router';
 
-app.disable('x-powered-by')
+export const app = express();
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.disable('x-powered-by');
 
-// middlewares
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// middlewares ======================>
 const log = (req, res, next) => {
-  console.log('logger', req.body, res.statusCode)
+  console.log('logger', req.body, res.statusCode);
 
   // pass back any data to the 'next' controller
-  req.mydata = 'mydata from logger'
+  req.mydata = 'mydata from logger';
+  next();
+};
 
-  next()
-}
-// routes
-app.get('/', log, (req, res) => {
-  const status = JSON.stringify(res.statusCode, null, 2)
-  res.send({ message: `Index route, status: ${status}` })
-})
-
-app.get('/data', log, (req, res) => {
-  const status = JSON.stringify(res.statusCode, null, 2)
-  const dataFromLogger = req.mydata
-
-  res.send({
-    message: `Data route, status: ${status}, dataFromLogger ${dataFromLogger}`
-  })
-})
-
-app.post('/data', (req, res) => {
-  //   const status = JSON.stringify(res.statusCode, null, 2)
-  //   const body = JSON.stringify(req.body, null, 2)
-
-  res.send(req.body)
-})
+// mount routers ======================>
+app.use('/api/item', itemRouter);
 
 export const start = () => {
   app.listen(3000, () => {
-    console.log('Server started on port 3000')
-  })
-}
+    console.log('Server started on port 3000');
+  });
+};
